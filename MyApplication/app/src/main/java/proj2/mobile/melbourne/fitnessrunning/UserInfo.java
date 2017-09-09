ecec707ae55dfb008858dff00ae67bbbbe8098f5
Da Huo <huodamelbourne@gmail.com>
2017-09-09 23:29:15 +1000

    09092330

---
+++ MyApplication/app/src/main/java/proj2/mobile/melbourne/fitnessrunning/UserInfo.java
@@ -0,0 +1,15 @@
+package proj2.mobile.melbourne.fitnessrunning;
+
+/**
+ * Created by rionay on 2017/9/9.
+ */
+
+public class UserInfo {
+
+
+    @com.google.gson.annotations.SerializedName("username")
+    private String UserName;
+    @com.google.gson.annotations.SerializedName("password")
+    private String Password;
+
+}
