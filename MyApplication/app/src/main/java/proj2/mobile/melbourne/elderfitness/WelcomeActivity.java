6b1a7a973de9a1b5bdfe5d5d6a7ecc6cce8b83c9
yuhcenQ <qiaoycmelbourne@gmail.com>
2017-10-03 00:14:38 +1100

    adjust guide page

--- MyApplication/app/src/main/java/proj2/mobile/melbourne/elderfitness/WelcomeActivity.java
+++ MyApplication/app/src/main/java/proj2/mobile/melbourne/elderfitness/WelcomeActivity.java
@@ -46,8 +46,8 @@ public class WelcomeActivity extends Activity {
 
     private void startMainActivity(){
         // elapsedRealtime() 从开机到现在的毫秒数
-        Random random = new Random(SystemClock.elapsedRealtime());
-        mIVEntry.setImageResource(Imgs[random.nextInt(Imgs.length)]);
+//        Random random = new Random(SystemClock.elapsedRealtime());
+//        mIVEntry.setImageResource(Imgs[random.nextInt(Imgs.length)]);
 
         // timer: 延迟执行某个操作，第一个参数表示延迟时间，第二个参数是时间单位
         Observable.timer(1000, TimeUnit.MILLISECONDS)
