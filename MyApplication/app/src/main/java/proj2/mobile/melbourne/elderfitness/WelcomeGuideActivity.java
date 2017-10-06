08fd4dcf86e74d7cc3f3c0d25644982b5ca26d54
yuhcenQ <qiaoycmelbourne@gmail.com>
2017-10-06 19:02:47 +1100

    add readme

--- MyApplication/app/src/main/java/proj2/mobile/melbourne/elderfitness/WelcomeGuideActivity.java
+++ MyApplication/app/src/main/java/proj2/mobile/melbourne/elderfitness/WelcomeGuideActivity.java
@@ -24,14 +24,11 @@ public class WelcomeGuideActivity extends Activity implements View.OnClickListen
     ViewPager vp;
     @BindView(R.id.ll)
     LinearLayout ll;
-    // 引导页图片资源
     private static final int[] pics = {
             R.layout.guide_view1,
             R.layout.guide_view2,
             R.layout.guide_view3};
-    // 底部小点图片
     private ImageView[] dots;
-    // 记录当前选中位置
     private int currentIndex;
 
     @Override
@@ -41,7 +38,7 @@ public class WelcomeGuideActivity extends Activity implements View.OnClickListen
         ButterKnife.bind(this);
 
         List<View> views = new ArrayList<>();
-        // 初始化引导页视图列表
+        // initial the guide pages list:
         for (int i = 0; i < pics.length; i++) {
             View view = LayoutInflater.from(this).inflate(pics[i], null);
             if (i == pics.length - 1) {
@@ -56,24 +53,24 @@ public class WelcomeGuideActivity extends Activity implements View.OnClickListen
         initDots(ll);
     }
 
+    // initial the dot list:
     private void initDots(LinearLayout ll) {
         dots = new ImageView[pics.length];
-        // 循环取得小点图片
+
         for (int i = 0; i < pics.length; i++) {
-            // 得到一个LinearLayout下面的每一个子元素
             dots[i] = (ImageView) ll.getChildAt(i);
-            dots[i].setEnabled(false);// 都设为灰色
+            dots[i].setEnabled(false);
             dots[i].setOnClickListener(this);
-            dots[i].setTag(i);// 设置位置tag，方便取出与当前位置对应
+            dots[i].setTag(i);
         }
         currentIndex = 0;
-        dots[currentIndex].setEnabled(true); // 初始化，设置为白色，即选中状态
+        dots[currentIndex].setEnabled(true);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        // 如果切换到后台，就设置下次不进入功能引导页
+        // if switch to background, it won't show guide pages next time:
         SPUtils.put(WelcomeGuideActivity.this, SPUtils.FIRST_OPEN, false);
         finish();
     }
@@ -89,9 +86,7 @@ public class WelcomeGuideActivity extends Activity implements View.OnClickListen
         setCurDot(position);
     }
 
-    /**
-     * 设置当前view
-     */
+    // set current view:
     private void setCurView(int position) {
         if (position < 0 || position >= pics.length) {
             return;
@@ -99,9 +94,7 @@ public class WelcomeGuideActivity extends Activity implements View.OnClickListen
         vp.setCurrentItem(position);
     }
 
-    /**
-     * 设置当前指示点
-     */
+    // set current dot:
     private void setCurDot(int position) {
         if (position < 0 || position > pics.length || currentIndex == position) {
             return;
